eecec7b1a8de3a36497315295a24912ad756281d
TobiasP <t.petri91@gmx.de>
2021-03-07 19:26:06 +0100

    bug fixing

--- app/run.py
+++ app/run.py
@@ -33,7 +33,7 @@ df = pd.read_sql_table("messages_disaster", engine)
 
 
 # load model
-model = joblib.load("../models/classifier.pkl")
+model = joblib.load("../model/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
